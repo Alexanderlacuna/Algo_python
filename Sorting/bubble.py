6947c2e22b0d83591931a488e6b58d559b4a2b18
Alexanderkabui <alexanderkabua@gmail.com>
2020-12-28 20:20:36 +0300

    pep8 formatting
    delete comments

--- Sorting/bubble.py
+++ Sorting/bubble.py
@@ -1,18 +1,18 @@
-def swap(slices,greater,lesser):
-	temp=slices[greater]
-	slices[greater]=slices[lesser]
-	slices[lesser]=temp
+def swap(slices, greater, lesser):
+    temp = slices[greater]
+    slices[greater] = slices[lesser]
+    slices[lesser] = temp
+
+
 def bubble_sort(slices):
-	swapped=True
+    swapped = True
 
-	while (swapped):
-		swapped=False;
+    while (swapped):
+        swapped = False
 
-		for i in  range(0,len(slices)-1):
-			if slices[i]>slices[i+1]:
-				swap(slices,i,i+1)
-				swapped=True
+        for i in range(0, len(slices)-1):
+            if slices[i] > slices[i+1]:
+                swap(slices, i, i+1)
+                swapped = True
 
-	return slices
-r=bubble_sort([4,3,2,1])
-print(r)
+    return slices
--- Sorting/test_bubble.py
+++ Sorting/test_bubble.py
@@ -1,13 +1,14 @@
 import unittest
 from bubble import bubble_sort
 
-class TestBubbleSort(unittest.TestCase):
-	def test_sorted(self):
-		_sorted=[1,2,3,4]
-		self.assertEqual(_sorted,bubble_sort(_sorted))
 
-	def test_bubble(self):
-		unsorted=[4,3,2,1,12]
-		_sorted=bubble_sort(unsorted)
-		expected_results=[1,2,3,4,12]
-		self.assertEqual(expected_results,_sorted)
+class TestBubbleSort(unittest.TestCase):
+    def test_sorted(self):
+        _sorted = [1, 2, 3, 4]
+        self.assertEqual(_sorted, bubble_sort(_sorted))
+
+    def test_bubble(self):
+        unsorted = [4, 3, 2, 1, 12]
+        _sorted = bubble_sort(unsorted)
+        expected_results = [1, 2, 3, 4, 12]
+        self.assertEqual(expected_results, _sorted)

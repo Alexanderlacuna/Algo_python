9445a6ab3e1ebda308538c60c8b06f0c5113c885
Alexanderkabui <alexanderkabua@gmail.com>
2020-12-31 14:00:09 +0300

    test github workflow

--- Sorting/test_bubble.py
+++ Sorting/test_bubble.py
@@ -3,6 +3,9 @@ from bubble import bubble_sort
 
 
 class TestBubbleSort(unittest.TestCase):
+
+	def test_workflow(self):
+		self.assertEqual(4,5)
     def test_sorted(self):
         _sorted = [1, 2, 3, 4]
         self.assertEqual(_sorted, bubble_sort(_sorted))
@@ -12,3 +15,6 @@ class TestBubbleSort(unittest.TestCase):
         _sorted = bubble_sort(unsorted)
         expected_results = [1, 2, 3, 4, 12]
         self.assertEqual(expected_results, _sorted)
+
+if __name__ == '__main__':
+	unittest.main()
